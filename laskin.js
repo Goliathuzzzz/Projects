e3eaf9b640520f5734fbac2f859647c11653075d
Goliathuzzzz <mlaakkonen@live.com>
2023-11-09 10:17:38 +0200

    Sivuilla tilastoja ja laskin laskee kumulatiivisen todennäköisyysjakauman.

--- laskin.js
+++ laskin.js
@@ -35,7 +35,7 @@ for (let j = 0; j < attempts; j++) {
 }
 
 const probability = (hits / attempts);
-const tulos = 100 * probability.toFixed(6) + '%';
+const tulos = 100 * probability.toFixed(4) + '%';
 
 // probability to get number x times in y rolls of 2 dice
 
@@ -43,39 +43,46 @@ const xinput = prompt('Montako kertaa luku tuli: ');
 const yinput = prompt('Montako kertaa noppia heitettiin: ');
 const x = parseInt(xinput);
 const y = parseInt(yinput);
+let totalp = 0
+for (let i = 0; i <= x; i++) {
+  totalp +=
+    (factorialize(y)/(factorialize(i) * factorialize(y - i)))
+    * (probability ** i) * ((1-probability) ** (y-i));
+}
 
-const probability2 =
-    (factorialize(y)/(factorialize(x) * factorialize(y - x)))
-    * (probability ** x) * ((1-probability) ** (y-x));
+// if total probability > 50%, take inverse probability
 
-const tulos2 = 100 * probability2.toFixed(6) + '%';
+if (totalp > 0.5) {
+  totalp = 1 - totalp;
+}
+const tulos2 = 100 * totalp.toFixed(4) + '%';
 
 document.querySelector('#todnak').innerHTML =
     tulos;
 
-if (probability2 >= droprate(1)) {
+if (totalp >= droprate(1)) {
   document.querySelector('#tulos').innerHTML =
       tulos2 + ': Näin kuului tapahtua.';
 }
-else if (droprate(1) > probability2 && probability2 >= droprate(2)) {
+else if (droprate(1) > totalp && totalp >= droprate(2)) {
   document.querySelector('#tulos').innerHTML =
       tulos2 + ': Ei tällaisesta voi valittaa. Valintojen syytä.';
 }
-else if (droprate(2) > probability2 && probability2 >= droprate(3)) {
+else if (droprate(2) > totalp && totalp >= droprate(3)) {
   document.querySelector('#tulos').innerHTML =
       tulos2 + ': Epätodennäköisempää, kuin että Vesa valittaisi jostain.';
 }
-else if (droprate(3) > probability2 && probability2 >= droprate(4)) {
+else if (droprate(3) > totalp && totalp >= droprate(4)) {
   document.querySelector('#tulos').innerHTML =
       tulos2 + ': Ihan semi unlucky/lucky.';
 }
-else if (droprate(4) > probability2 && probability2 >= droprate(5)) {
+else if (droprate(4) > totalp && totalp >= droprate(5)) {
   document.querySelector('#tulos').innerHTML =
-      tulos2 + ': Näistä tahtoo lähteä himaan. / Et voittanut taitojesi takia.';
+      tulos2 + ': Näistä tahtoo lähteä himaan. / Et onnistunut taitojesi takia.';
 }
-else if (droprate(5) > probability2 && probability2 >= droprate(6)) {
+else if (droprate(5) > totalp && totalp >= droprate(6)) {
   document.querySelector('#tulos').innerHTML =
-      tulos2 + ': https://findahelpline.com/fi/topics/suicidal-thoughts.';
+      tulos2 + ': https://findahelpline.com/fi/topics/suicidal-thoughts. / Olet niin onnekas, että joku antaa runtukepistä.';
 }
 else {
   document.querySelector('#tulos').innerHTML =
